f979d2f257cdf93143369adef9a4099440ea8ab9
Jyoti Patel <jyotipatel707172@gmail.com>
2025-09-01 00:40:28 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import {authDataContext} from './AuthContext.jsx'
 import axios from 'axios';
 import { io } from "socket.io-client";
 import { useNavigate } from 'react-router-dom';
- export let socket=io("https://linkedin-backend-vrmf.onrender.com") 
+ export let socket=io("http://localhost:8000") 
   
 export const userDataContext=createContext();
 
