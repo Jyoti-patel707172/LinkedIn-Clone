63daad48e212e532dae16b6b35d8820a6fd7fca6
Jyoti Patel <jyotipatel707172@gmail.com>
2025-10-14 14:48:20 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -30,7 +30,7 @@ export const signUp=async(req,res)=>{
      res.cookie('token', token,{
       httpOnly:true,
       maxAge:7*24*60*60*1000,
-      sameSite:"strict",
+      sameSite:"None",
       secure:process.env.NODE_ENV==="production"
      });
       let { password: pass, ...rest } = user._doc;
@@ -62,7 +62,7 @@ export const login=async(req,res)=>{
      res.cookie('token', token,{
       httpOnly:true,
       maxAge:7*24*60*60*1000,
-      sameSite:"strict",
+      sameSite:"None",
       secure:process.env.NODE_ENV==="production"
      });
       let { password: pass, ...rest } = user._doc;
