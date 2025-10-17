a80e301fb118df3174a917529df6580553cd41ac
Jyoti Patel <jyotipatel707172@gmail.com>
2025-10-17 13:46:46 +0530

    Update ConnectionButton.jsx

--- frontend/src/components/ConnectionButton.jsx
+++ frontend/src/components/ConnectionButton.jsx
@@ -5,7 +5,7 @@ import io from "socket.io-client"
 import { userDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 
-const socket = io("https://linkedin-clone-backend-hpn4.onrender.com", { withCredentials: true });
+const socket = io("https://linkedin-clone-backend1.onrender.com", { withCredentials: true });
 
 function ConnectionButton({userId}) {
   
